bb00bba87f8c8e600ae9abce55096a20da4142d2
xBananaKing79x <khakimov.t@gmail.com>
2025-04-06 20:00:51 +0300

    Создание методов приложения

---
+++ src/org/skypro/skyshop/App.java
@@ -0,0 +1,4 @@
+package org.skypro.skyshop;
+
+public class App {
+}
---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,21 @@
+package org.skypro.skyshop.product;
+
+public class Product {
+    private String productName;
+    private int productPrice;
+
+    public Product(String productName, int productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName () {
+        return productName;
+    }
+    public int getProductPrice() {
+        return productPrice;
+    }
+    public String toString() {
+        return productName + ": " + productPrice;
+    }
+}
