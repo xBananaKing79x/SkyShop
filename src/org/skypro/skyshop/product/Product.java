b68c4db78952e8f1e41c5cf7addb0708bc4ebb42
xBananaKing79x <khakimov.t@gmail.com>
2025-04-12 22:05:50 +0300

    Скорректировано наименование объекта класса Product

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,20 +1,16 @@
 package org.skypro.skyshop.product;
 
-public class Product {
+public abstract class Product {
     private String productName;
-    private int productPrice;
 
     public Product(String productName, int productPrice) {
         this.productName = productName;
-        this.productPrice = productPrice;
     }
     public String getProductName () {
         return productName;
     }
-    public int getProductPrice() {
-        return productPrice;
-    }
+    public abstract int getProductPrice();
     public String toString() {
-        return productName + ": " + productPrice;
+        return productName;
     }
 }
